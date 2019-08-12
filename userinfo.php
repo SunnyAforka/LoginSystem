7858275b2a7bdcf62b1f895c6d39efc3203a5ed5
Sunny <afokasunday@gmail.com>
2019-08-12 11:43:23 +0100

    Update userinfo.php
    userdata

--- userinfo.php
+++ userinfo.php
@@ -6,5 +6,5 @@ function getUsersData($id) {
 	$array = array();
 	$q = mysqli_query("SELECT * FROM users WHERE 'id' = "" ")
 }
-
+//Still working on this part of the project to display every infomation about user on the welcome page when the user login..
  ?>
